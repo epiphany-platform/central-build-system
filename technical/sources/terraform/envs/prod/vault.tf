7d7302d175b0e8e922608d40641a034eef08cfd0
DaOsie <damian.osienicki@gmail.com>
2020-12-02 17:27:50 +0100

    Move sensitive variables to azure key vault

---
+++ technical/sources/terraform/envs/prod/vault.tf
@@ -0,0 +1,28 @@
+data "azurerm_key_vault" "cbs" {
+  name                = "cbs-prod" //var.keyvault_name
+  resource_group_name = "cbs-tools-rg" //var.keyvault_rg
+}
+
+data "azurerm_key_vault_secret" "cbs_vault" {
+  for_each = toset( [
+    "address-space",
+    "address-prefixes",
+    "location",
+    "client-id",
+    "client-secret",
+    "tenant-id",
+    "aad-admin-groups",
+    "vm-rg-name",
+    "vm-vnet-id",
+    "vm-vnet-name",
+    "argo-prefix",
+    "tekton-prefix",
+    "domain",
+    "tekton-operator-container",
+    "harbor-prefix",
+    "harbor-tls-secret-name",
+    "harbor-storage-account-name"
+  ])
+  name = each.value
+  key_vault_id = data.azurerm_key_vault.cbs.id
+}
