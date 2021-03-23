1b38cf0b0f1beb35f4a643f71032de873570c4a7
Roman Sokalski <24roman12@gmail.com>
2021-03-23 09:18:58 +0100

    Revert "Tagging helps"
    This reverts commit b2ed213d3bf98f023dcee1d7cf8d473f70767c64.

--- sources/cbs_core/modules/k8s/api-gateway.tf
+++ sources/cbs_core/modules/k8s/api-gateway.tf
@@ -1,11 +1,8 @@
 locals {
-  backend_address_pool_name          = "backend_address_pool_name"
+  backend_address_pool_name_argo     = "argo_backend_addr_pool"
   http_setting_name                  = "http_setting_name"
-  frontend_ip_configuration_name     = "listener"
-  frontend_port_name                 = "https"
-  frontend_priv_pub_ip_configuration_name     = "listener_priv_pub"
-  frontend_priv_pub_port_name                 = "https"
-  frontend_pub_ip_configuration_name = "listener_pub"
+  frontend_ip_configuration_name_prv = "listener_priv"
+  frontend_ip_configuration_name_pub = "listener_pub"
   frontend_pub_port_name             = "https_pub"
 }
 
@@ -22,50 +19,46 @@ resource "azurerm_application_gateway" "api_gw" {
   name                = var.name
   resource_group_name = var.rg_name
 
-  backend_address_pool {
-    name = local.backend_address_pool_name
-  }
-
-  frontend_ip_configuration {
-    name                 = local.frontend_priv_pub_ip_configuration_name
-    public_ip_address_id = azurerm_public_ip.app_gw_pub_ip.id
-  }
-
   frontend_port {
-    name = local.frontend_priv_pub_port_name
+    name = "httpPort"
     port = 80
   }
 
   frontend_ip_configuration {
-    name                          = local.frontend_ip_configuration_name
+    name                          = local.frontend_ip_configuration_name_prv
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Static"
     private_ip_address            = cidrhost(var.appgw_subnet_cidr[0], 37)
   }
 
-  frontend_port {
-    name = local.frontend_port_name
-    port = 80
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name_pub
+    public_ip_address_id = azurerm_public_ip.app_gw_pub_ip.id
   }
 
   gateway_ip_configuration {
     name      = var.name
     subnet_id = var.subnet_id
   }
+
   http_listener {
-    frontend_ip_configuration_name = local.frontend_ip_configuration_name
-    frontend_port_name             = local.frontend_port_name
-    name                           = local.frontend_ip_configuration_name
-    host_name                      = "${var.tekton_prefix}.${var.domain}"
+    name                           = local.frontend_ip_configuration_name_prv
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name_prv
+    frontend_port_name             = "httpPort"
+    host_name                      = "${var.argo_prefix}.${var.domain}"
     protocol                       = "Http"
   }
 
   request_routing_rule {
-    name                       = local.frontend_ip_configuration_name
+    name                       = "${local.frontend_ip_configuration_name_prv}_argo_rule"
     rule_type                  = "Basic"
-    http_listener_name         = local.frontend_ip_configuration_name
+    http_listener_name         = local.frontend_ip_configuration_name_prv
     backend_http_settings_name = local.http_setting_name
-    backend_address_pool_name  = local.backend_address_pool_name
+    backend_address_pool_name  = local.backend_address_pool_name_argo
+  }
+  
+  backend_address_pool {
+    name = local.backend_address_pool_name_argo
   }
 
   backend_http_settings {
@@ -73,7 +66,7 @@ resource "azurerm_application_gateway" "api_gw" {
     name                  = local.http_setting_name
     port                  = 80
     protocol              = "http"
-    request_timeout       = 30
+    request_timeout       = 3
   }
 
   sku {
@@ -86,6 +79,10 @@ resource "azurerm_application_gateway" "api_gw" {
     max_capacity = 2
   }
 
-  # After creation ingress controller is managing it
+  tags = {
+    CreatedWhen = timestamp()
+  }
+
+  # After creation, the ingress controller is managing it
   lifecycle { ignore_changes = all }
 }
