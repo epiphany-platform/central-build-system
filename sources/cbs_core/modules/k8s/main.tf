9ffbb9cf7facd8750fe236b7fe7e755a4c71ab64
Roman Sokalski <24roman12@gmail.com>
2021-03-25 13:51:57 +0100

    OIDC secret configuration is now automatic

--- sources/cbs_core/modules/k8s/main.tf
+++ sources/cbs_core/modules/k8s/main.tf
@@ -36,6 +36,14 @@ resource "null_resource" "argocd" {
     command = "kubectl apply --kubeconfig tf_kubeconfig -f ../modules/k8s/manifests/argocd.yaml"
   }
 
+  depends_on = [null_resource.kube_config_create, null_resource.OIDC_secret, kubernetes_namespace.argocd_ns, kubernetes_config_map.argocd_cm]
+}
+
+resource "null_resource" "OIDC_secret" {
+  provisioner "local-exec" {
+    command = "cat <<EOF | kubectl apply --kubeconfig tf_kubeconfig -f - \n${templatefile("${path.module}/manifests/argocd-secret.tmpl", { CLIENT_SECRET = base64encode(var.client_secret) })}"
+  }
+
   depends_on = [null_resource.kube_config_create, kubernetes_namespace.argocd_ns, kubernetes_config_map.argocd_cm]
 }
 
@@ -81,5 +89,5 @@ resource "null_resource" "kube_config_destroy" {
     always_run = timestamp()
   }
 
-  depends_on = [null_resource.argocd, null_resource.tekton_crd, null_resource.tekton-triggers, null_resource.kube_config_create, null_resource.operator, null_resource.nginx_ingress]
+  depends_on = [null_resource.argocd, null_resource.OIDC_secret, null_resource.tekton_crd, null_resource.tekton-triggers, null_resource.kube_config_create, null_resource.operator, null_resource.nginx_ingress]
 }
