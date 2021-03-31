04860aca57f2a0764d990e679ff3f7e15e836624
DaOsie <damian.osienicki@gmail.com>
2021-03-31 10:44:05 +0200

    Upgrade k8s to 1.18.14

--- technical/sources/terraform/modules/aks/variables.tf
+++ technical/sources/terraform/modules/aks/variables.tf
@@ -62,7 +62,7 @@ variable "private_cluster" {
 
 variable "kubernetes_version" {
   type        = string
-  default     = "1.17.13"
+  default     = "1.18.14"
   description = "your kubernetes version"
 }
 
